9bcff51022a8d0aa153d0c78c5b581a596650074
chalermpols <36227290+chalermpols@users.noreply.github.com>
2018-02-08 16:49:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -24,13 +24,13 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 			
-			if $text = "1"{
+			//if $text = "1"{
 				// Build message to reply back
-				$messages = [
-				'type' => 'text',
-				'text' => "แจ้งเคลม"
-				];
-			}
+			//	$messages = [
+			//	'type' => 'text',
+			//	'text' => "แจ้งเคลม"
+			//	];
+			//}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
